71798f09af84d5d2c16add0a74ff6b117964f9ca
daya950 <daya950@gmail.com>
2018-08-14 16:07:25 +0530

    Update RequiredJs.js

--- RequiredJs.js
+++ RequiredJs.js
@@ -1,27 +1,25 @@
-<script>
-    $(document).ready(function() {
-        var arr = [];
-        $('span.form-option-variant--color').each(function() {
-            arr.push($(this).attr("title").toLowerCase());
-        });
-        
-        if(arr.length > 0) {
-            showHide(arr[0]);
-        }
-        
-        $('span.form-option-variant--color').click(function() {
-            showHide($(this).attr("title").toLowerCase());
-        }); 
+$(document).ready(function() {
+    var arr = [];
+    $('span.form-option-variant--color').each(function() {
+        arr.push($(this).attr("title").toLowerCase());
     });
-    
-    function showHide(varCol) {
-        $('ul.productView-thumbnails li a img').each(function(){ 
-            var str1 = $(this).attr("alt").toLowerCase();
-            if(str1.indexOf(varCol) == -1) {
-                $(this).parent().parent().hide();
-            } else {
-                $(this).parent().parent().show();
-            }
-        });
+
+    if(arr.length > 0) {
+        showHide(arr[0]);
     }
-</script>
+
+    $('span.form-option-variant--color').click(function() {
+        showHide($(this).attr("title").toLowerCase());
+    }); 
+});
+
+function showHide(varCol) {
+    $('ul.productView-thumbnails li a img').each(function(){ 
+        var str1 = $(this).attr("alt").toLowerCase();
+        if(str1.indexOf(varCol) == -1) {
+            $(this).parent().parent().hide();
+        } else {
+            $(this).parent().parent().show();
+        }
+    });
+}
